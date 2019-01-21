4956be3988b9cfef792ed877b322b5f3758c3440
HassanRazaMuhammadTufail <hassanrazamohammadtufail@gmail.com>
2019-01-21 14:43:21 +0500

    Practice

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to React Native!</Text>
+        <Text>Hassan Raza</Text>
+        <Text>Roll no: 894</Text>
+        <Text>Batch 4.2</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
